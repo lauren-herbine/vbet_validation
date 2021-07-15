87a1622a012d45de9eef7300a51e16f9dd9ba17e
Lauren Herbine <73319684+lauren-herbine@users.noreply.github.com>
2021-07-15 15:42:58 -0600

    Lauren's edits
    See my comment in issue [#310](https://github.com/Riverscapes/riverscapes-tools/issues/310) for more about the edits

--- vbet_validation.R
+++ vbet_validation.R
@@ -1,5 +1,5 @@
 # Load data and packages
-evidence <-read.csv("/research/transform_fun/vbet_evidence_01.csv")
+evidence <-read.csv("vbet_evidence_02.csv")
 
 library(ggplot2)
 library(DBI)
@@ -7,13 +7,14 @@ library(tidyverse)
 library(RSQLite)
 
 #Change data type of userid to factor
-evidence$userid = as.factor(evidence$userid)
+# evidence$userid = as.factor(evidence$userid) 
+#(taking this out since it wasn't working)
 
 # Ignore for now. Creating SQL database
 vbet_evidence_db <-dbConnect(RSQLite::SQLite(), "vbet_evidence.db")
 
 # Ignore for now. Creating evidence table
-dbExecute(evidence_db, "CREATE TABLE evidence (
+dbExecute(vbet_evidence_db, "CREATE TABLE evidence (
           observationid,
           categoryid,
           userid,
@@ -25,8 +26,8 @@ dbExecute(evidence_db, "CREATE TABLE evidence (
           );")
 
 # Ignore this for now. Loading csv into 'observations' table:
-observations <- read.csv("data/vbet_evidence_01.csv", 
-                    stringsAsFactors = FALSE)
+observations <- read.csv("vbet_evidence_02.csv", 
+                         stringsAsFactors = FALSE)
 names(observations)
 
 dbWriteTable(vbet_evidence_db, "evidence", evidence, append = TRUE)
@@ -38,73 +39,73 @@ evidence
 
 # Box plots
 
-evidence %>%
-  ggplot(mapping = aes(x = slope)) +
+observations %>%
+  ggplot(mapping = aes(x = Slope)) +
   geom_boxplot() +
   labs (x = "Slope (degrees)")
 
-evidence %>%
-  ggplot(mapping = aes(x = hand)) +
+observations %>%
+  ggplot(mapping = aes(x = HAND)) +
   geom_boxplot() +
   labs (x = "HAND (m)")
 
-evidence %>%
+observations %>%
   ggplot(mapping = aes(x = distance)) +
   geom_boxplot() +
   labs (x = "Distance from Nearest Drainage Cell")
 
-evidence %>%
-  ggplot(mapping = aes(x = twi)) +
+observations %>%
+  ggplot(mapping = aes(x = TWI)) +
   geom_boxplot() +
   labs (x = "Topograpgic Wetness Index")
 
 # Histograms
 
-evidence %>%
+observations %>%
   ggplot(mapping = aes(x = slope)) +
   geom_histogram() +
   labs (x = "Slope (degrees)")
 
-evidence %>%
+observations %>%
   ggplot(mapping = aes(x = hand)) +
   geom_histogram() +
   labs (x = "HAND (m)")
 
-evidence %>%
+observations %>%
   ggplot(mapping = aes(x = distance)) +
   geom_histogram() +
   labs (x = "Distance from Nearest Drainage Cell")
 
-evidence %>%
-  ggplot(mapping = aes(x = twi)) +
+observations %>%
+  ggplot(mapping = aes(x = TWI)) +
   geom_histogram() +
   labs (x = "Topograpgic Wetness Index")
 
 # Experimenting with color-coding by users in the box plot.. still working this
 # out
 
-evidence %>%
-  ggplot(mapping = aes(x = slope, y = categoryid)) +
+observations %>%
+  ggplot(mapping = aes(x = Slope, y = categoryid)) +
   geom_point () +
   labs(x = "Slope", y = "Likelihood", color = "User") +
   theme_minimal() +
   theme(legend.position = "right")
 
-evidence %>%
-  ggplot(mapping = aes(x = slope, y = categoryid, color = userid)) +
+observations %>%
+  ggplot(mapping = aes(x = Slope, y = categoryid, color = userid)) +
   geom_point () +
   labs(x = "Slope", y = "Likelihood", color = "User") +
   theme_minimal() +
   theme(legend.position = "right") 
 
-evidence %>%
-ggplot(mapping = aes(x = slope, color = userid)) +
+observations %>%
+  ggplot(mapping = aes(x = Slope, color = userid)) +
   geom_boxplot() +
   labs (x = "Slope (degrees)")
 
 # Still in progress of converting these over to new column names
 evidence %>%
-ggplot(mapping = aes(x = slope, y = likelihood, color = recorder_id)) +
+  ggplot(mapping = aes(x = slope, y = likelihood, color = recorder_id)) +
   geom_point () +
   labs(x = " ", y = "Likelihood", color = "User") +
   theme_minimal() +
@@ -113,23 +114,23 @@ ggplot(mapping = aes(x = slope, y = likelihood, color = recorder_id)) +
 # Histogram for Evidence Rasters
 
 ggplot(data = observations, 
-       mapping = aes(x = slope)) +
+       mapping = aes(x = Slope)) +
   geom_histogram(bins = 10) +
   labs (x = "Slope (degrees)", y = "Count") +
   theme_light()
 
 ggplot(data = observations, 
-       mapping = aes(x = hand)) +
+       mapping = aes(x = HAND)) +
   geom_histogram(bins = 10) +
   labs (x = "HAND (m)", y = "Count")
 
 ggplot(data = observations, 
-       mapping = aes(x = distance)) +
+       mapping = aes(x = ChannelDist)) +
   geom_histogram(bins = 10) +
   labs (x = "Distance from Drainage Cell (m)", y = "Count")
 
 ggplot(data = observations, 
-       mapping = aes(x = twi)) +
+       mapping = aes(x = TWI)) +
   geom_histogram(bins = 10) +
   labs (x = "Topographic Wetness Index (TWI)", y = "Count")
 
@@ -158,5 +159,3 @@ min(evidence$twi)
 mean(evidence$twi)
 mode(evidence$twi)
 median(evidence$twi)
-
-
